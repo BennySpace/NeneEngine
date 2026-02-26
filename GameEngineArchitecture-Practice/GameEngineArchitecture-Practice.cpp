825407521f23e176a0665ecb5852b8b7a52112ea
Daniil Vlasov <356550@edu.itmo.ru>
2026-02-26 20:03:55 +0300

    Add project files.

---
+++ GameEngineArchitecture-Practice/GameEngineArchitecture-Practice.cpp
@@ -0,0 +1,12 @@
+﻿// GameEngineArchitecture-Practice.cpp : Defines the entry point for the application.
+//
+
+#include "GameEngineArchitecture-Practice.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
